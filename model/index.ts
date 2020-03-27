6a97440c5c739fb3ff18235d7ec0ca0a2a04bd6f
MishaNavrotsky <misha172xdd@gmail.com>
2020-03-27 18:44:24 +0300

    1

--- model/index.ts
+++ model/index.ts
@@ -1,62 +1,47 @@
-import {
-    Engine
-} from "@babylonjs/core/Engines/engine";
-import {
-    Scene
-} from "@babylonjs/core/scene";
-import {
-    Vector3
-} from "@babylonjs/core/Maths/math";
-import {
-    FreeCamera
-} from "@babylonjs/core/Cameras/freeCamera";
-import {
-    HemisphericLight
-} from "@babylonjs/core/Lights/hemisphericLight";
-import {
-    Mesh
-} from "@babylonjs/core/Meshes/mesh";
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3 } from "@babylonjs/core/Maths/math";
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
-import {
-    Button,
-    AdvancedDynamicTexture
-} from "@babylonjs/gui"
+import { Button, AdvancedDynamicTexture } from "@babylonjs/gui"
 
-import * as BABYLON from "@babylonjs/core"
- 
+import { CannonJSPlugin, PhysicsImpostor } from "@babylonjs/core"
+
 
 import "@babylonjs/core/Meshes/meshBuilder";
-import "@babylonjs/core/Materials" 
+import "@babylonjs/core/Materials"
 import "@babylonjs/gui"
 
 window.onload = function () {
-    const canvas = <HTMLCanvasElement>document.getElementById("canvas");
-    const engine = new Engine(canvas);
+  const canvas = <HTMLCanvasElement>document.getElementById("canvas");
+  const engine = new Engine(canvas);
 
 
-    var scene = new Scene(engine);
-    scene.enablePhysics(new Vector3(0,-9.81,0),new BABYLON.CannonJSPlugin());
+  var scene = new Scene(engine);
+  scene.enablePhysics(new Vector3(0, -9.81, 0), new CannonJSPlugin());
 
-    var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+  var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 
-    camera.setTarget(Vector3.Zero());
+  camera.setTarget(Vector3.Zero());
 
-    camera.attachControl(canvas, true);
+  camera.attachControl(canvas, true);
 
-    var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+  var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 
-    light.intensity = 0.7;
+  light.intensity = 0.7;
 
-    var sphere = Mesh.CreateSphere("sphere1", 160, 2, scene);
-    sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+  var sphere = Mesh.CreateSphere("sphere1", 160, 2, scene);
+  sphere.physicsImpostor = new PhysicsImpostor(sphere, PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
 
-    sphere.position.y = 1.2;
+  sphere.position.y = 1.2;
 
-    var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
-    ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.PlaneImpostor, { mass: 0}, scene)
+  var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
+  ground.physicsImpostor = new PhysicsImpostor(ground, PhysicsImpostor.PlaneImpostor, { mass: 0 }, scene)
 
 
-    engine.runRenderLoop(() => {
-        scene.render();
-    });
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 }
